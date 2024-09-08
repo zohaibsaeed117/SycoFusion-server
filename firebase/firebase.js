d614acf485cb8b1387647d863b0dd0ade2c28ece
Zohaib <zohaib.saeed1259@gmail.com>
2024-09-08 20:46:24 +0500

    fixed the enviroment variables of firebase

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,10 +1,21 @@
 // config/firebaseConfig.js
 const admin = require('firebase-admin');
-const serviceAccount = require('../config/syco-fusion-firebase-adminsdk-w9wgz-1df272d0d1.json');
 require('dotenv').config();
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        "type": process.env.TYPE,
+        "project_id": process.env.PROJECT_ID,
+        "private_key_id": process.env.PRIVATE_KEY_ID,
+        "private_key": process.env.PRIVATE_KEY,
+        "client_email": process.env.CLIENT_EMAIL,
+        "client_id": process.env.CLIENT_ID,
+        "auth_uri": process.env.AUTH_URI,
+        "token_uri": process.env.TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+        "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+        "universe_domain": process.env.UNIVERSE_DOMAIN
+    }),
     storageBucket: process.env.STORAGE_BUCKET // Use uppercase for env variable convention
 });
 
